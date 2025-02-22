31203c5bbf3ff986bf0f38737e53c4f01725962f
Owen Luts <owenlutsdev@gmail.com>
2025-02-22 18:06:58 -0500

    removed some bloat

--- desktop/dist/index.js
+++ desktop/dist/index.js
@@ -2,7 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const electron_1 = require("electron");
 const node_path_1 = require("node:path");
-const database_1 = require("./database");
 const utils_1 = require("./utils");
 function createWindow() {
     const win = new electron_1.BrowserWindow({
@@ -27,16 +26,6 @@ function createWindow() {
 }
 electron_1.app.whenReady().then(async () => {
     await (0, utils_1.prepareNext)("./frontend", 4444);
-    await (0, utils_1.initLogs)();
-    await database_1.sequelize
-        .sync({
-        logging: true,
-        alter: true,
-        force: false,
-    })
-        .then(() => {
-        console.log("Database synced");
-    });
     createWindow();
     electron_1.app.on("activate", () => {
         if (electron_1.BrowserWindow.getAllWindows().length === 0)
@@ -46,19 +35,4 @@ electron_1.app.whenReady().then(async () => {
 electron_1.app.on("window-all-closed", () => {
     if (process.platform !== "darwin")
         electron_1.app.quit();
-});
-electron_1.ipcMain.on("addUser", async (event, data) => {
-    await database_1.User.create(data)
-        .then((data) => {
-        event.returnValue = {
-            error: false,
-            data: data.dataValues,
-        };
-    })
-        .catch((error) => {
-        event.returnValue = {
-            error: true,
-            data: error,
-        };
-    });
 });
