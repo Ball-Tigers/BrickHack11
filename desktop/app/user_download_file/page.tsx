c00acfd1a386c1c0e5cf7269cb11b473ae717d93
Owen Luts <owenlutsdev@gmail.com>
2025-02-23 02:16:37 -0500

    jaysen being a real one

--- desktop/app/user_download_file/page.tsx
+++ desktop/app/user_download_file/page.tsx
@@ -1,22 +1,25 @@
 'use client'
-import { useState } from "react"
+// import { useState } from "react"
 
 export default function UserDownloadFile() {
-    const [hasAuth, setHasAuth] = useState(true)
+    // const [hasAuth, setHasAuth] = useState(true)
 
     return (
         <div className="flex flex-row w-full h-full">
             <div className='w-1/2 flex justify-center items-center'>
                 {
-                    hasAuth ? (
+                    true ? (
                         <a className='button'>Link is here</a>
                     ) : (
                         <div>hasNotAuth</div>
                     )
                 }
             </div>
-            <div className='w-1/2 bg-accent home-clip-right flex justify-center items-center'>
-                <p className='text-7xl w-1 flex-wrap text-secondary flex justify-center text-center'>Download File</p>
+            <div className='w-1/2 bg-accent home-clip-right flex flex-col justify-center items-center'>
+                <img 
+                    src='jafe_small.png'
+                />
+                <p className='text-7xl w-1 flex-wrap text-secondary flex justify-center text-center'>Download</p>
             </div>
             {/* <h1>User Download File</h1>
             <p>If has access, button to download</p>
--- desktop/dist/utils/isDev.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isDev = void 0;
-const electron_log_1 = __importDefault(require("electron-log"));
-exports.isDev = process.argv.some((str) => str == "--dev");
-electron_log_1.default.info("IS_DEV: ", exports.isDev);
