e229d5f2ef06044df1ff31e905bc4536144148b3
Dtay234 <dn5600@rit.edu>
2025-02-23 05:53:44 -0500

    fixed invite

--- client/components/dashboard_permission_group.tsx
+++ client/components/dashboard_permission_group.tsx
@@ -23,7 +23,7 @@ export default function AdminDashboardGroup({title, devices}: Props) {
                     <span>{title}</span>
                     <div className="flex justify-right space-x-5">
                         {
-                            toggle && <button className="">Invite</button>
+                            toggle && <button onClick={async (e) => {e.stopPropagation(); await navigator.clipboard.writeText((await createInvite(title)).inviteCode)}}>Invite</button>
                         }
                         <svg data-accordion-icon className="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5 5 1 1 5"/>
